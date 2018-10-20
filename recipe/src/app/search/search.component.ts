d1198bb50ed2ae749761db64f1ba804035955a64
Srinath Reddy Isukapalli <344584@narayanahealth.org>
2018-10-20 19:33:08 +0530

    analyzed route params

--- recipe/src/app/search/search.component.ts
+++ recipe/src/app/search/search.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { SearchService } from '../service/search.service';
-import { ActivatedRoute, Router } from "@angular/router";
 
 @Component({
   selector: 'app-search',
@@ -8,7 +8,7 @@ import { ActivatedRoute, Router } from "@angular/router";
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent implements OnInit {
- 
+
   loading;
   data;
   doSearchUsingPromise(searchTerm) {
@@ -16,26 +16,52 @@ export class SearchComponent implements OnInit {
     this.searchServe.doSearchList(searchTerm).then((res) => {
       this.loading = false, this.data = res;
     },
-    (err) => {
-      console.log(err);
-    }
-  );
-  }
-  
-  constructor( private searchServe: SearchService, private activatedRoute: ActivatedRoute, private router: Router ) {
-    this.activatedRoute.params.subscribe(
-      params => {
-        //url should be like http://localhost:4200/search;term=123;name=sri
-        //console.log(params.term) //will give all params 
-        this.doSearchUsingPromise(params.term);
+      (err) => {
+        console.log(err);
       }
-    )
+    );
+  }
+
+  constructor(private searchServe: SearchService, private activatedRoute: ActivatedRoute, private router: Router) {
+    /*
+      route param we will get in 2 ways 1 is by subscribing params and 2nd one is by using snapshot.
+      whenever route param dont change then we can use snapshot
+      in below case whenever search happen url param will chnage. so we should use subscribe
+     */
+    // angula2
+    /* this.activatedRoute.params.subscribe(
+        params => {
+          // url should be like http://localhost:4200/search;term=123;name=sri
+          // console.log(params.term) //will give all params
+          this.doSearchUsingPromise(params.term);
+        }
+      );
+    */
+    // angiular 4 above
+    this.activatedRoute.paramMap.subscribe(
+      params => {
+        // url should be like http://localhost:4200/search;term=123;name=sri
+        // console.log(params.term) //will give all params
+        this.doSearchUsingPromise(params.get('term'));
+      }
+    );
+
+    // angular 2
+    // const searchURLTerm = this.activatedRoute.snapshot.params.term;
+    // above angular 4 instaed of params we should replace paramMap.get(param name) ex:
+    /*const searchURLTerm = this.activatedRoute.snapshot.paramMap.get('term');
+    this.doSearchUsingPromise(searchURLTerm);*/
   }
 
   ngOnInit() {
   }
 
   doSearch(searchTerm) {
-    this.router.navigate(['artist', {term: searchTerm}]);
+    /*
+      we will pass 1st param as route,  next params as comma separated required parameters
+      and after that we will pass optional parameters as a object
+      example route: this.router.navigate(['artist', "requiredParam1", "requiredParam2", {term: searchTerm, name: srinath}]);
+    */
+    this.router.navigate(['artist', { term: searchTerm }]);
   }
 }
