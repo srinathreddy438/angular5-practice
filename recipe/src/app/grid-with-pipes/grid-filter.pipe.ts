97a3227933a1e3f18a0cd423771d5673833f9136
Srinath Reddy Isukapalli <344584@narayanahealth.org>
2018-10-21 17:12:25 +0530

    formatted code

--- recipe/src/app/grid-with-pipes/grid-filter.pipe.ts
+++ recipe/src/app/grid-with-pipes/grid-filter.pipe.ts
@@ -4,13 +4,16 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class FilterPipe implements PipeTransform {
     transform(items: any[], searchProperty: string, searchRent: string, locality: string): any[] {
-        if (!items) return [];
-        return items.filter(it => {
-            const propertySize = it.propertySize <= searchProperty;
-            const rent = it.rent < searchRent;
-            const searchPlace = it.locality && it.locality.toLowerCase().includes(locality);
-            return (propertySize && rent && searchPlace);   
+        if (!items) {
+            return [];
+        } else {
+            return items.filter(it => {
+                const propertySize = it.propertySize <= searchProperty;
+                const rent = it.rent < searchRent;
+                const searchPlace = it.locality && it.locality.toLowerCase().includes(locality);
+                return (propertySize && rent && searchPlace);
 
-        });
+            });
+        }
     }
 }
